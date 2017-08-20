a4bb38acc59b473d094ba8135c4e8f0df6d24ff9
Danielle Beyer <dwishnuff@gmail.com>
2017-08-20 08:59:12 -0700

    finalized tables with totals and styleguide

--- fish.js
+++ fish.js
@@ -14,6 +14,8 @@ var location1 = {
   max: 88,
   avg: 5.2,
   cookiesbyhour: [],
+
+//calculate estimated volume by hour
   calcEstVolume: function () {
     for (var indexHours = 0; indexHours<hours.length; indexHours++){
       location1.cookiesbyhour.push (Math.floor(randomNum(location1.min, location1.max) * location1.avg));
@@ -43,8 +45,6 @@ var location1 = {
   buildTable: function () {
     for (var index=0; index<hours.length; index++){
       document.getElementById('location1').innerHTML += location1.locationInfoRow(index);
-
-
     }
     document.getElementById('location1').innerHTML += location1.totSalesRow();
   }
@@ -57,34 +57,57 @@ location1.buildTable();
 
 
 
-// buildTable();
+
 var location2 = {
   name: 'Portland Airport',
   min: 6,
   max: 24,
   avg: 1.2,
   cookiesbyhour: [],
+
+  //calculate estimated volume by hour
   calcEstVolume: function () {
     for (var indexHours = 0; indexHours<hours.length; indexHours++){
       location2.cookiesbyhour.push (Math.floor(randomNum(location2.min, location2.max) * location2.avg));
     }
   },
-  locationInfoRow: function  (index) {
-    var rowData = "<tr><td>" + hours[index] + "</td>";
-    rowData += "<td>" + location2.cookiesbyhour[index] + "</td></tr>";
-    return rowData;
-  },
-  buildTable: function () {
-    for (var index=0; index<hours.length; index++){
 
-      document.getElementById('location2').innerHTML += location2.locationInfoRow(index);
+    //calculate total sales
+    TotSales: 0,
+    calcTotSales: function() {
+      for (var index = 0; index<location2.cookiesbyhour.length; index++){
+        location2.TotSales += location2.cookiesbyhour[index]
+      }
+    },
+
+    //create total sales row for table
+    totSalesRow: function(){
+      var totRow="<tr><td>" + "TOTAL: " +"</td>";
+      totRow +="<td>" + location2.TotSales +"</td></tr>";
+      return totRow;
+    },
+    //create row data for table
+    locationInfoRow: function  (index) {
+      var rowData = "<tr><td>" + hours[index] + "</td>";
+      rowData += "<td>" + location2.cookiesbyhour[index] + "</td></tr>";
+      return rowData;
+    },
+
+//build table using rowData and totSalesRow
+    buildTable: function () {
+      for (var index=0; index<hours.length; index++){
+        document.getElementById('location2').innerHTML += location2.locationInfoRow(index);
+      }
+      document.getElementById('location2').innerHTML += location2.totSalesRow();
     }
-  }
-};
+  };
 
-document.getElementById('locName2').innerHTML = location2.name
-location2.calcEstVolume();
-location2.buildTable();
+//run each function
+  document.getElementById('locName2').innerHTML = location2.name
+  location2.calcEstVolume();
+  location2.calcTotSales();
+  location2.buildTable();
+
 
 var location3 = {
   name: 'Washington Square',
@@ -92,26 +115,46 @@ var location3 = {
   max: 38,
   avg: 1.9,
   cookiesbyhour: [],
+//calculate estimated volume by hour
   calcEstVolume: function () {
     for (var indexHours = 0; indexHours<hours.length; indexHours++){
       location3.cookiesbyhour.push (Math.floor(randomNum(location3.min, location3.max) * location3.avg));
     }
   },
+  //calculate total sales
+  TotSales: 0,
+  calcTotSales: function() {
+    for (var index = 0; index<location3.cookiesbyhour.length; index++){
+      location3.TotSales += location3.cookiesbyhour[index]
+    }
+  },
+
+  //create total sales row for table
+  totSalesRow: function(){
+    var totRow="<tr><td>" + "TOTAL: " +"</td>";
+    totRow +="<td>" + location3.TotSales +"</td></tr>";
+    return totRow;
+  },
+  //create row data for table
   locationInfoRow: function  (index) {
     var rowData = "<tr><td>" + hours[index] + "</td>";
     rowData += "<td>" + location3.cookiesbyhour[index] + "</td></tr>";
     return rowData;
   },
+
+//build table using rowData and totSalesRow
   buildTable: function () {
     for (var index=0; index<hours.length; index++){
-
       document.getElementById('location3').innerHTML += location3.locationInfoRow(index);
     }
+    document.getElementById('location3').innerHTML += location3.totSalesRow();
   }
 };
 
+//run each function
 document.getElementById('locName3').innerHTML = location3.name
 location3.calcEstVolume();
+location3.calcTotSales();
 location3.buildTable();
 
 var location4 = {
@@ -120,27 +163,47 @@ var location4 = {
   max: 48,
   avg: 3.3,
   cookiesbyhour: [],
-  calcEstVolume: function () {
-    for (var indexHours = 0; indexHours<hours.length; indexHours++){
-      location4.cookiesbyhour.push (Math.floor(randomNum(location4.min, location4.max) * location4.avg));
-    }
-  },
-  locationInfoRow: function  (index) {
-    var rowData = "<tr><td>" + hours[index] + "</td>";
-    rowData += "<td>" + location4.cookiesbyhour[index] + "</td></tr>";
-    return rowData;
-  },
-  buildTable: function () {
-    for (var index=0; index<hours.length; index++){
+  //calculate estimated volume by hour
+    calcEstVolume: function () {
+      for (var indexHours = 0; indexHours<hours.length; indexHours++){
+        location4.cookiesbyhour.push (Math.floor(randomNum(location4.min, location4.max) * location4.avg));
+      }
+    },
+    //calculate total sales
+    TotSales: 0,
+    calcTotSales: function() {
+      for (var index = 0; index<location4.cookiesbyhour.length; index++){
+        location4.TotSales += location4.cookiesbyhour[index]
+      }
+    },
 
-      document.getElementById('location4').innerHTML += location4.locationInfoRow(index);
-    }
-  }
-};
+    //create total sales row for table
+    totSalesRow: function(){
+      var totRow="<tr><td>" + "TOTAL: " +"</td>";
+      totRow +="<td>" + location4.TotSales +"</td></tr>";
+      return totRow;
+    },
+    //create row data for table
+    locationInfoRow: function  (index) {
+      var rowData = "<tr><td>" + hours[index] + "</td>";
+      rowData += "<td>" + location4.cookiesbyhour[index] + "</td></tr>";
+      return rowData;
+    },
 
-document.getElementById('locName4').innerHTML = location4.name
-location4.calcEstVolume();
-location4.buildTable();
+  //build table using rowData and totSalesRow
+    buildTable: function () {
+      for (var index=0; index<hours.length; index++){
+        document.getElementById('location4').innerHTML += location4.locationInfoRow(index);
+      }
+      document.getElementById('location4').innerHTML += location4.totSalesRow();
+    }
+  };
+
+  //run each function
+  document.getElementById('locName4').innerHTML = location4.name
+  location4.calcEstVolume();
+  location4.calcTotSales();
+  location4.buildTable();
 
 var location5 = {
   name: 'Pearl District',
@@ -148,24 +211,44 @@ var location5 = {
   max: 24,
   avg: 2.6,
   cookiesbyhour: [],
-  calcEstVolume: function () {
-    for (var indexHours = 0; indexHours<hours.length; indexHours++){
-      location5.cookiesbyhour.push (Math.floor(randomNum(location5.min, location5.max) * location5.avg));
-    }
-  },
-  locationInfoRow: function  (index) {
-    var rowData = "<tr><td>" + hours[index] + "</td>";
-    rowData += "<td>" + location5.cookiesbyhour[index] + "</td></tr>";
-    return rowData;
-  },
-  buildTable: function () {
-    for (var index=0; index<hours.length; index++){
+  //calculate estimated volume by hour
+    calcEstVolume: function () {
+      for (var indexHours = 0; indexHours<hours.length; indexHours++){
+        location5.cookiesbyhour.push (Math.floor(randomNum(location5.min, location5.max) * location5.avg));
+      }
+    },
+    //calculate total sales
+    TotSales: 0,
+    calcTotSales: function() {
+      for (var index = 0; index<location5.cookiesbyhour.length; index++){
+        location5.TotSales += location5.cookiesbyhour[index]
+      }
+    },
 
-      document.getElementById('location5').innerHTML += location5.locationInfoRow(index);
-    }
-  }
-};
+    //create total sales row for table
+    totSalesRow: function(){
+      var totRow="<tr><td>" + "TOTAL: " +"</td>";
+      totRow +="<td>" + location5.TotSales +"</td></tr>";
+      return totRow;
+    },
+    //create row data for table
+    locationInfoRow: function  (index) {
+      var rowData = "<tr><td>" + hours[index] + "</td>";
+      rowData += "<td>" + location5.cookiesbyhour[index] + "</td></tr>";
+      return rowData;
+    },
 
-document.getElementById('locName5').innerHTML = location5.name
-location5.calcEstVolume();
-location5.buildTable();
+  //build table using rowData and totSalesRow
+    buildTable: function () {
+      for (var index=0; index<hours.length; index++){
+        document.getElementById('location5').innerHTML += location5.locationInfoRow(index);
+      }
+      document.getElementById('location5').innerHTML += location5.totSalesRow();
+    }
+  };
+
+  //run each function
+  document.getElementById('locName5').innerHTML = location5.name
+  location5.calcEstVolume();
+  location5.calcTotSales();
+  location5.buildTable();
